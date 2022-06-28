f263586bbb8d2a42534fad41a00d3d549e671b21
benoit-bail <nbhep@yahoo.fr>
2022-06-28 16:12:56 +0200

    28juin2022

---
+++ excercice00/calculatrice.js
@@ -0,0 +1,70 @@
+let i = 0;
+
+    while(i < 10)
+{
+    document.write("bbb");
+    i++;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*let sun = (a, b) =>
+a = 10;
+b = 21;
+{
+if 
+    (a%b == 0)
+    return OK
+    console.log (10 % 21);
+
+    else {
+        (a%b != 0)
+        return not OK
+        console.log (10 % 21);
+    }
+}
+
+document.write (a, b);
+*/
+
+
+
+
+
+
+
+
+/*dodo = 40
+
+function affichage()
+{
+    let data = 20; 
+    didi = 30;
+    function interieur ()
+    {
+        document.write (data, didi, dodo);
+            }
+            interieur ();
+}
+affichage();*/
